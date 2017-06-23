1f1a5fd3dc263cc06d04bd2c297c168cc6641453
benImran <benchaa.said-medjahed@hetic.net>
2017-06-23 17:43:40 +0200

    Erreur

---
+++ src/AppBundle/Security/LoginFormAuthenticator.php
@@ -0,0 +1,110 @@
+<?php
+
+
+namespace AppBundle\Security;
+
+
+use AppBundle\AppBundle;
+use AppBundle\Entity\User;
+use AppBundle\Form\LoginForm;
+use Doctrine\ORM\EntityManager;
+use Symfony\Component\Form\FormFactoryInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\RouterInterface;
+use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
+use Symfony\Component\Security\Core\Exception\AuthenticationException;
+use Symfony\Component\Security\Core\User\UserInterface;
+use Symfony\Component\Security\Core\User\UserProviderInterface;
+use Symfony\Component\Security\Guard\Authenticator\AbstractFormLoginAuthenticator;
+use Symfony\Component\Security\Http\Util\TargetPathTrait;
+
+class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
+{
+    /**
+     * @var FormFactoryInterface
+     */
+    private $formFactory;
+    /**
+     * @var EntityManager
+     */
+    private $em;
+    /**
+     * @var RouterInterface
+     */
+    private $router;
+
+    /**
+     * LoginFormAuthenticator constructor.
+     */
+    public function __construct(FormFactoryInterface $formFactory, EntityManager $em, RouterInterface $router)
+    {
+
+        $this->formFacory = $formFactory;
+        $this->em = $em;
+        $this->router = $router;
+    }
+
+    public function getCredentials(Request $request)
+    {
+       $isLoginSubmit = $request->getPathInfo() == '/login' && $request->isMethod('POST');
+
+        if (!$isLoginSubmit) {
+            return;
+        }
+
+        $form = $this->formFactory->create(LoginForm::class);
+        $form->handleRequest($request);
+
+        $data = $form->getData();
+
+        return $data;
+
+
+    }
+
+    public function getUser($credentials, UserProviderInterface $userProvider)
+    {
+        $username = $credentials['_username'];
+
+        return $this->em->getRepository('AppBundle::User')
+            ->findOneBy(['username' => $username]);
+    }
+
+    protected function getLoginUrl()
+    {
+        return $this->router->generate('security_login');
+
+    }
+
+    public function checkCredentials($credentials, UserInterface $user)
+    {
+        $password = $credentials['password'];
+
+        if ($password == 'benben') {
+            return true;
+        }
+
+        return false;
+    }
+
+    use TargetPathTrait;
+
+    public function onAuthenticationSuccess(Request $request, TokenInterface $token, $providerKey)
+    {
+        $targetPath = $this->getTargetPath($request->getSession(), $providerKey);
+
+        if (!$targetPath) {
+            $targetPath = $this->router->generate('homepage');
+        }
+
+        return new RedirectResponse($targetPath);
+    }
+
+    function __call($name, $arguments)
+    {
+        // TODO: Implement __call() method.
+    }
+
+
+}
