cd6545b9358eec3f122af957e82e7950a80cfa1e
Sebastien Baudray <sebastien.baudray@hetic.net>
2017-06-26 01:46:51 +0200

    footer + routes

--- src/AppBundle/Controller/PageController.php
+++ src/AppBundle/Controller/PageController.php
@@ -16,9 +16,49 @@ class PageController extends Controller
     }
 
     /**
-     * @Route("/about", name="about")
+     * @Route("/a-propos", name="about")
      */
     public function aboutAction()
+    {
+        return $this->render('pages/about.html.twig', []);
+    }
+
+    /**
+     * @Route("/mission", name="mission")
+     */
+    public function missionAction()
+    {
+        return $this->render('pages/about.html.twig', []);
+    }
+
+    /**
+     * @Route("/projet", name="project")
+     */
+    public function projectAction()
+    {
+        return $this->render('pages/about.html.twig', []);
+    }
+
+    /**
+     * @Route("/soutenir", name="support")
+     */
+    public function supportAction()
+    {
+        return $this->render('pages/about.html.twig', []);
+    }
+
+    /**
+     * @Route("/contact", name="contact")
+     */
+    public function contactAction()
+    {
+        return $this->render('pages/about.html.twig', []);
+    }
+
+    /**
+     * @Route("/partenaires", name="partners")
+     */
+    public function partnersAction()
     {
         return $this->render('pages/about.html.twig', []);
     }
