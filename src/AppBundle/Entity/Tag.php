410a7f045342c9c5529ef695a8499673642d3f51
Sebastien Baudray <sebastien.baudray@hetic.net>
2017-06-28 11:32:43 +0200

    tag

--- src/AppBundle/Entity/Tag.php
+++ src/AppBundle/Entity/Tag.php
@@ -38,6 +38,11 @@ class Tag
         $this->articles = new ArrayCollection();
     }
 
+    public function __toString()
+    {
+        return $this->getName();
+    }
+
     public function getId()
     {
         return $this->id;
